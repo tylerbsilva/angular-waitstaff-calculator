16f44aaf05b8c24eebf71419eb124f34c9fecb41
Tyler Silva <tylerbsilva@gmail.com>
2015-07-01 21:54:17 -0500

    Started adding functions to add tip and total

--- js/app.js
+++ js/app.js
@@ -1,10 +1,32 @@
 var app = angular.module('waitstaffCalc', []);
 
 app.controller('calcController', function($scope){
-  $scope.subtotal = 56.7;
-  $scope.tip = 23.6;
-  $scope.total = 1456.23;
-  $scope.tipTotal = 123.43;
-  $scope.mealCount = 24;
-  $scope.averageTip = 125.24;
+  $scope.mealCount = 0;
+  $scope.tipTotal = 0;
+
+  $scope.submit= function(){
+    if ($scope.inputNumbers.$valid){
+      console.log($scope.data);
+      // Find subtotal with tax
+      $scope.subtotal = $scope.tax($scope.data.mealPrice, $scope.data.taxRate);
+      // Calculate tip amount
+      $scope.tipAmount = $scope.tip($scope.subtotal, $scope.data.tipPercentage);
+      // Update Total
+      $scope.total = $scope.subtotal + $scope.tipAmount;
+      // Add tips to total tips
+      $scope.tipTotal += $scope.tipAmount;
+      // Add 1 to the meal count
+      $scope.mealCount++;
+    }
+  };
+
+  $scope.tax = function(price, taxRate){
+    var tax = price*(taxRate/100);
+    return price + tax;
+  }
+  $scope.tip = function(price, tipPercent){
+    return price*(tipPercent/100);
+  }
+
+
 });
