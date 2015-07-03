e4573300ae52524ceda0687f2b8aff40796400b5
Tyler Silva <tylerbsilva@gmail.com>
2015-07-03 17:36:15 -0500

    Added a full reset, and fixed calculations

--- js/app.js
+++ js/app.js
@@ -6,7 +6,6 @@ app.controller('calcController', function($scope){
 
   $scope.submit= function(){
     if ($scope.inputNumbers.$valid){
-      console.log($scope.data);
       // Find subtotal with tax
       $scope.subtotal = $scope.tax($scope.data.mealPrice, $scope.data.taxRate);
       // Calculate tip amount
@@ -17,15 +16,38 @@ app.controller('calcController', function($scope){
       $scope.tipTotal += $scope.tipAmount;
       // Add 1 to the meal count
       $scope.mealCount++;
+      // Average Tip
+      $scope.averageTip = $scope.avgTipPerMeal($scope.tipTotal, $scope.mealCount);
+      // Clear input values
+      $scope.resetForm();
+    } else {
+      alert('Oops! Something went wrong!');
     }
   };
-
+  $scope.resetForm = function(){
+    $scope.data.mealPrice = '';
+    $scope.data.taxRate = '';
+    $scope.data.tipPercentage = '';
+  }
   $scope.tax = function(price, taxRate){
     var tax = price*(taxRate/100);
     return price + tax;
   }
   $scope.tip = function(price, tipPercent){
     return price*(tipPercent/100);
+  }
+  $scope.avgTipPerMeal = function(tips, mealCount){
+    return tips/mealCount;
+  }
+
+  $scope.resetAll = function(){
+    $scope.resetForm();
+    $scope.subtotal = '';
+    $scope.tipAmount = '';
+    $scope.total = '';
+    $scope.tipTotal = '';
+    $scope.mealCount = '';
+    $scope.averageTip = '';
   }
 
 
